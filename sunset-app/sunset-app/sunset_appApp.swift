95d256ccc20325a463e43d78fc0051a4da536601
meganma <meganma45@gmail.com>
2023-04-22 13:07:10 -0700

    Created xcode file

---
+++ sunset-app/sunset-app/sunset_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunset_appApp.swift
+//  sunset-app
+//
+//  Created by Megan Ma on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct sunset_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sunset-app/sunset-appUITests/sunset_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sunset_appUITestsLaunchTests.swift
+//  sunset-appUITests
+//
+//  Created by Megan Ma on 4/22/23.
+//
+
+import XCTest
+
+final class sunset_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
